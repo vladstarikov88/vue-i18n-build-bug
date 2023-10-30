dcdf88e8085d9800ac147a6aa7beb09fc3284bfe
Vladislav <vlad.starikov.88@gmail.com>
2023-10-30 12:29:14 +0300

    vue-i18n bug

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { i18n } from './i18n';
+
+export const app = createApp(App)
+  .use(i18n);
+
+app.mount('#app');
